beb22ac85ec4943af693fb211c14b3108b2f0bac
khemmanat <khemmanayt@gmail>
2023-01-17 17:32:24 +0700

    khemmanat

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -3,8 +3,26 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
+      T temp;
+	  for(int i=1;i<N;i++){
+        for(int j=i;j>0;j--){
+             if(d[j]>d[j-1]){
+				 temp = d[j-1];
+				 d[j-1]=d[j];
+				 d[j]=temp;
+			    
+			 }else{break; }
+			 
+				 }
+	cout<<"Pass "<<i<<":";
+	for(int k=0;k<N;k++){			 
+    cout <<d[k]<<" ";
+		}
+		cout<<endl;
+                              }
+	  
+    }
 
-}
 
 int main(){
 	int a[10] = {12,25,30,44,2,0,4,7,55,25};
--- lab15_2.cpp
+++ lab15_2.cpp
@@ -19,3 +19,42 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+void inputMatrix(double A[N][N]){
+          for(int i=0;i<N;i++){
+              cout<<"Row "<<i+1<<": "; 
+             for(int j=0;j<N;j++){
+            cin>>A[i][j];
+                
+          
+                                 }  
+                            }
+}
+
+                               
+
+
+ void findLocalMax(const double A[N][N], bool B[N][N]){
+      for(int i=0;i<N;i++){
+          for(int j=0;j<N;j++){
+              if(i==0 || j==0 || i==N-1||j==N-1){
+				  B[i][j]= 0;
+			  }else{
+				if(A[i][j]>=A[i-1][j] && A[i][j]>=A[i][j+1]&&A[i][j]>=A[i+1][j]&&A[i][j]>=A[i][j-1]){
+					B[i][j]=1;
+				}else{B[i][j]=0;}
+			  }
+                               }
+    }
+}
+
+void showMatrix(const bool A[N][N]){
+        for(int i=0;i<N;i++){
+             
+             for(int j=0;j<N;j++){
+                cout <<A[i][j]<<" ";
+          
+                                 }  
+            cout<<endl;
+                            }
+
+                                    }
